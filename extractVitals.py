2047fb687c6b58555c2372d4fc26a33cd4239f93
ejbnnett <ethan-bennett@uiowa.edu>
2021-03-28 15:34:43 -0500

    bl

--- extractVitals.py
+++ extractVitals.py
@@ -3,6 +3,8 @@ import re
 def valueFromUnit(value,unit):
     try:
         value = float(value)
+    except:
+        pass
     if unit in {'c','celsius'}:
         return  value*9/5+32
     elif unit in {"kg","kilo","kilogram"}:
@@ -19,7 +21,7 @@ def extractVitalSignFromText(text):
                        "o2","blood oxygen","oxygen saturation","spo2","oxygen", "oximeter","sao2","o2 sat","ox sat"
                        "respirations","resperation rate", "resperation","rp"
                        "blood pressure","bp"
-                       "weight","wt",
+                       "weight","wt",'weighs'
                        "height","ht",
                        "body mass index","bmi",
                        "hematocrit","packed cell volume","htc","pcv"
@@ -41,16 +43,23 @@ def extractVitalSignFromText(text):
         "pulse": "beats per minute",
         "resperation rate": "resperation rate"
         }
+    
     forExpr = "|".join(vitalIndicators+list(vitalAcronyms.keys())).lower()
+    #What does a number look like?
+    signValue = r"""[0-9]{1,3}((/|\.)[0-9]{1,3}([0-9]|/{1,3})?)?"""
     #expr = r"""(P<indicator>""" +forExpr+ """)\s?(is|at|:)?\s?(P<val>[0-9]{1,3}(/[0-9]{1,3})?)"""
-    expr = r"""(^|\s)(?P<indicator>""" +forExpr+ """)\s?('s|is|at|lab|was|
-are|test|level|\(|\)|,|stat|-|=|rate|amount|of|reading|:)*\s?(?P<val>[0-9]{1,3}((/|\.)[0-9]{1,3})?)\s?\
+    #This expression is for indicators in the format indicator __ format
+    expr = r"""(^|\s)(?P<indicator>""" +forExpr+ """)\s?('s|is|at|about|lab|was|
+are|test|level|\(|\)|,|stat|-|=|rate|amount|of|reading|:){0,5}\s?(?P<val>"""+signValue+r""")\s?\
 (?P<unit>mg/hg|celsius|c|farenheit|f|kg|kilo|lb|measured|today|hormone|pound|pounds|degree|bpm)?"""
     #print(expr)
     matches = list(re.finditer(expr,text))
+    #Sometimes, indicators are after
+    expr = r"""(^|\s)(?P<val>[0-9]{1,3}((/|\'|\.|\s)([0-9]|/){1,3})?)\s+(degree|of|at)?\s*(?P<indicator>fever|lb|inches|in|bpm)"""
+    matches.add(re.finditer(expr,text))
+    expr = r"""(physical exam|exam|vital sign|vital signs)\s+(""" +signValue+ r"""|in|'|inches|feet|\s|lb|pound|pounds)+(P<val>"""+signValue+ r""")"""
     indicatorDictionary = vitalAcronyms
-    sometimesAfter = ["fever", "pounds"]
-    assert all( for k in indicatorDictionary.keys())
+    assert all([k in indicatorDictionary for k in vitalIndicators])
     for m in matches:
         value = m.group("val")
         indicator = m.group("indicator")
